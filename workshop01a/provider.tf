8ee7141e2919f6d915be2e4a86a42a70b6019787
ominbruce <brucetheflame@outlook.com>
2021-09-27 16:40:23 +0800

    added provisioner

--- workshop01a/provider.tf
+++ workshop01a/provider.tf
@@ -5,17 +5,22 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "2.12.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
   }
+
 }
 
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
-variable "do_token" {
-  type      = string
-  sensitive = true
-}
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "local" {
+
 }
