5cd5cd78074c3e0643ff05305d4882af7ff3d1b2
ominbruce <brucetheflame@outlook.com>
2021-09-29 16:09:51 +0800

    Added s3 backend read

--- readStateFromS3/resources.tf
+++ readStateFromS3/resources.tf
@@ -11,14 +11,18 @@ data "digitalocean_image" "mysql8" {
   name = "mysql8"
 }
 
-resource "digitalocean_droplet" "yoursql" {
-  image    = data.digitalocean_image.mysql8.id
-  name     = "yoursql"
-  region   = "sgp1"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [data.digitalocean_ssh_key.aipc.id]
+data "terraform_remote_state" "yoursql" {
+  backend = "s3"
+  config = {
+    endpoint                    = "sgp1.digitaloceanspaces.com"
+    key                         = "workshop03-tfprovision/terraform.tfstate"
+    bucket                      = "autospace"
+    region                      = "sgp1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
 }
-
-output "yoursql_ip" {
-  value = digitalocean_droplet.yoursql.ipv4_address
+output "database_endpoint" {
+  value = data.terraform_remote_state.yoursql.config
 }
